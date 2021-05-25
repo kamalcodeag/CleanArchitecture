551c47c933eb178d8505be913a8f0258fd58b826
Kamal Pasha <kamalpasha.ag@outlook.com>
2021-05-25 17:15:12 +0400

    "Clean.Arch.API" - Controllers added

---
+++ CleanArch/CleanArch.API/Controllers/SingerController.cs
@@ -0,0 +1,30 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SingerController : ControllerBase
+    {
+        private readonly ISingerService _singerService;
+
+        public SingerController(ISingerService singerService)
+        {
+            _singerService = singerService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllSingers()
+        {
+            SingerViewModel singerViewModel = _singerService.GetSingers();
+            return Ok(singerViewModel);
+        }
+    }
+}
---
+++ CleanArch/CleanArch.API/Controllers/TrackController.cs
@@ -0,0 +1,29 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TrackController : ControllerBase
+    {
+        private readonly ITrackService _trackService;
+
+        public TrackController(ITrackService trackService)
+        {
+            _trackService = trackService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTracks()
+        {
+            TrackViewModel trackViewModel = _trackService.GetTracks();
+            return Ok(trackViewModel);
+        }
+    }
+}
